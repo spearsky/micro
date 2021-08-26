de0b725fe3cb43d770e65a3779e7f63a4b23c720
spearsky <spearsky@gmail.com>
2021-08-26 10:31:54 +0900

    3-3 IndexController posts/save

--- src/main/java/com/test/springboot/study/web/IndexController.java
+++ src/main/java/com/test/springboot/study/web/IndexController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
     TODO
     Controller > ControllerTest 단위테스트를 만든다.
     디렉토리 구조가 같아야한다.
+
+3-3 /posts/save 링크(get)를 만났을 때 처리할 매핑 정보를 추가해준다
  */
 @Controller
 public class IndexController {
@@ -20,5 +22,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
